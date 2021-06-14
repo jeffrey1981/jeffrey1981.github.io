0a9bc2e6d513dc7fb0e406e48cfd677e1b58282f
Jeffrey Steven <jeffreysteven@Jeffreys-MBP.attlocal.net>
2021-06-14 08:08:15 -0700

    second commit

---
+++ test_jeffrey/main.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById("video-modal");
+var btn = document.getElementById("video-modal-trigger");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+  iframe = document.getElementById('youtube-video-iframe') ?
+  iframe.contentWindow.postMessage('{"event": "command", "func":"' +  'pauseVideo' + '", "args": ""}', '*');
+}
+
+span.onclick = function() {
+  closeModal()
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+   closeModal()
+  }
+}
